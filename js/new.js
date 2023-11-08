4900358ee3e6d5f9bb1dcdb2466a15d63477e577
이주성 <142555219+Ggomabandal@users.noreply.github.com>
2023-11-09 02:54:58 +0900

    Add files via upload

--- js/new.js
+++ js/new.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $("section#new div.wrap div.container div.hidden ul.list > li ul.imgcon > li:nth-child(1)").addClass("zup");
-    $("section#new div.wrap div.container div.hidden ul.list > li ul.imgcon > li:nth-child(1)").mouseenter(function(){
+    $("section#new div.wrap div.container div.hidden ul.list li.product ul.imgcon li.imgbox:nth-child(1)").addClass("zup");
+    $("section#new div.wrap div.container div.hidden ul.list li.product ul.imgcon li.imgbox:nth-child(1)").mouseenter(function(){
         $(this).removeClass("zup");
     });
-    $("section#new div.wrap div.container div.hidden ul.list > li ul.imgcon > li:nth-child(2)").mouseleave(function(){
-        $("section#new div.wrap div.container div.hidden ul.list > li ul.imgcon > li:nth-child(1)").addClass("zup");
+    $("section#new div.wrap div.container div.hidden ul.list li.product ul.imgcon li.imgbox:nth-child(2)").mouseleave(function(){
+        $("section#new div.wrap div.container div.hidden ul.list li.product ul.imgcon li.imgbox:nth-child(1)").addClass("zup");
     });
 });
